6e00b8b1c01f21ac1767f76681efa19658a2a68b
SB <sb@macs-MacBook-Pro-3.local>
2025-08-06 12:09:31 -0700

    Publish Pap photo

---
+++ photography/snu-stan-topez/index.m
@@ -0,0 +1,7 @@
++++
+title       = "Snu Stan Tropez"
+date        = 2024-10-12          # keep whatever date you prefer
+draft       = false
+cover = { image = "cover.jpg
+", alt = "Snu Stan Topez" }
++++
